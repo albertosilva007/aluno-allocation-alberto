87c675e333f82c4483a99d23886d7c22553f5412
José Alberto Claudino Silva <pr.josealbertoibc@gmail.com>
2024-02-27 11:52:25 -0300

    Add project

---
+++ src/test/java/com/project/professor/allocation/aluno/ProfessorAllocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
